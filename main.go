11ac6ceda01e746a532cfb765ade851df210262f
rui.zheng <rui.zheng@tcl.com>
2014-02-11 17:24:19 +0800

    add gzip

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/codegangsta/martini"
 	"github.com/garyburd/redigo/redis"
 	"github.com/ginuerzh/drivetoday/controllers"
+	"github.com/martini-contrib/gzip"
 	"log"
 	"net/http"
 	"os"
@@ -18,6 +19,7 @@ func init() {
 func classic() *martini.ClassicMartini {
 	r := martini.NewRouter()
 	m := martini.New()
+	m.Use(gzip.All())
 	m.Use(martini.Logger())
 	m.Use(controllers.RedisLoggerHandler)
 	m.Use(martini.Recovery())
